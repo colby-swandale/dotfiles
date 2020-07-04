271dade3e183aff4d205f44b9ea9f313cb1cb6a7
Colby Swandale <me@colby.fyi>
2020-07-04 15:08:30 +1000

    Add support for installing dotfiles on linux

---
+++ install.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+set -euo pipefail
+
+BREW=/usr/local/bin/brew
+
+pretty_print() {
+  printf "\n%b\n" "$1"
+}
+
+if ! command -v brew &>/dev/null; then
+  pretty_print "Insalling Homebrew.."
+  bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+else
+  pretty_print "You already have Homebrew installed...good job!"
+fi
+
+pretty_print "Install homebrew packages.."
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  BUNDLE_FILE=Brewfile.darwin
+elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
+  BUNDLE_FILE=Brewfile.linux
+fi
+
+$BREW bundle --file $BUNDLE_FILE
+
+pretty_print "Cleaning up.."
+
+$BREW cask cleanup
+$BREW cleanup
+
+pretty_print "Running post install.."
+
+. "post-install.sh"
--- post-install.sh
+++ post-install.sh
@@ -2,12 +2,20 @@
 
 set -euo pipefail
 
+STOW=/usr/local/bin/stow 
 DOCKUTIL=/usr/local/bin/dockutil
 
-$DOCKUTIL --remove all
-$DOCKUTIL --add /Applications/Firefox.app
-$DOCKUTIL --add /System/Applications/Music.app
-$DOCKUTIL --add /Applications/Things3.app
-$DOCKUTIL --add /Applications/1Password\ 7.app
-$DOCKUTIL --add /Applications/Bear.app
-$DOCKUTIL --add '~/Downloads' --view grid --display folder
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  stow zsh git asdf ssh hyper
+
+  # $DOCKUTIL --remove all
+  # $DOCKUTIL --add /Applications/Firefox.app
+  # $DOCKUTIL --add /System/Applications/Music.app
+  # $DOCKUTIL --add /Applications/Things3.app
+  # $DOCKUTIL --add /Applications/1Password\ 7.app
+  # $DOCKUTIL --add /Applications/Bear.app
+  # $DOCKUTIL --add '~/Downloads' --view grid --display folder
+elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
+  stow zsh git asdf
+  chsh -s $(which zsh)
+fi
