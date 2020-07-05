ff770a53cef0b1bcb083ce67bc2b4776202b37b2
Colby Swandale <me@colby.fyi>
2020-07-05 20:27:23 +1000

    Auto install homebrew

--- install.sh
+++ install.sh
@@ -9,7 +9,8 @@ pretty_print() {
 
 if ! command -v brew &>/dev/null; then
   pretty_print "Insalling Homebrew.."
-  bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+  echo | bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)" > /dev/null
+  if [ $? -eq 0 ]; then echo 'OK'; else echo 'Oh no...'; fi
 else
   pretty_print "You already have Homebrew installed...good job!"
 fi
